88e017b507d660c9d6c248ad970c466169b1e15f
Msarchitect83 <nagaraju.techlead@gmail.com>
2025-03-30 18:58:01 +0530

    Add files via upload

---
+++ src/main/java/com/ecom/ecomauthserver/EcomAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecom/ecomauthserver/EcomAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecom.ecomauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
